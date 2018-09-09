b8935766b7171c33a011bad9d70ac7bccea14b36
Peeter Mäeots <maeots.peeter@gmail.com>
2018-09-09 22:43:01 +0300

    completed "fizzBuzzer" tests and passed all tests

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,45 @@
+// import Chai expect into variable
+const expect = require('chai').expect;
+
+// import fizzBuzzer
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for 'fizzBuzzer' function
+describe('fizzBuzzer tests', function() {
+
+    // test for normal cases
+    it('should be a number divisible by 15 returning \'fizz-buzz\'', function() {
+        [15, 30, 150, 3000].forEach( input => {
+            expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+        });
+    });
+
+    it('should be a number divisible by 5 returning \'buzz\'', function() {
+        [5, 10, 20, 205].forEach( input => { 
+            expect(fizzBuzzer(input)).to.equal('buzz');
+        });
+    });
+
+    it('should be a number divisible by 3 returning \'fizz\'', function() {
+        [3, 9, 36, 363].forEach( input => { 
+            expect(fizzBuzzer(input)).to.equal('fizz');
+        });
+    });
+
+    it('should be a number not divisible by 15, 5 or 3', function() {
+        const normalCases = [4, 11, 19];
+        normalCases.forEach( input => {
+            expect(fizzBuzzer(input)).to.equal(input);
+        });
+    });
+
+    // test for edge cases
+    it('should raise error if input is not a number', function() {
+        const badInputs = [false, '3', true, 'busted'];
+        badInputs.forEach( item => {
+            expect( function() {
+                fizzBuzzer(item);
+            }).to.throw(Error); 
+        });
+    });
+});
