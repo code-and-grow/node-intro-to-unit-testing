5bb5f8b7c02a94942a7c6c2c673137cac0a9fc4b
Peeter Mäeots <maeots.peeter@gmail.com>
2018-09-10 15:15:19 +0300

    replaced arrow functions to regular ones

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -9,33 +9,33 @@ describe('fizzBuzzer tests', function() {
 
     // test for normal cases
     it('should be a number divisible by 15 returning \'fizz-buzz\'', function() {
-        [15, 30, 150, 3000].forEach( input => {
+        [15, 30, 150, 3000].forEach( function (input) {
             expect(fizzBuzzer(input)).to.equal('fizz-buzz');
         });
     });
 
     it('should be a number divisible by 5 returning \'buzz\'', function() {
-        [5, 10, 20, 205].forEach( input => { 
+        [5, 10, 20, 205].forEach( function (input) { 
             expect(fizzBuzzer(input)).to.equal('buzz');
         });
     });
 
     it('should be a number divisible by 3 returning \'fizz\'', function() {
-        [3, 9, 36, 363].forEach( input => { 
+        [3, 9, 36, 363].forEach( function (input) { 
             expect(fizzBuzzer(input)).to.equal('fizz');
         });
     });
 
     it('should be a number not divisible by 15, 5 or 3', function() {
-        [4, 11, 19].forEach( input => {
+        [4, 11, 19].forEach( function (input) {
             expect(fizzBuzzer(input)).to.equal(input);
         });
     });
 
     // test for edge cases
     it('should raise error if input is not a number', function() {
-        [false, '3', true, 'busted'].forEach( input => {
-            expect( input => {
+        [false, '3', true, 'busted'].forEach( function (input) {
+            expect( function (input) {
                 fizzBuzzer(input);
             }).to.throw(Error); 
         });
